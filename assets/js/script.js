929607c314a41d48664c83b337e79ddb8fcc517c
Abraham Galindo <abraham.garo@gmail.com>
2020-02-13 20:40:59 -0600

    Colors added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,20 @@
-// set hambuguer actions
+
+/** Begin Functions */
+
+/**
+ * Return fist element
+ */
+const queryElement = (selector) => {
+    const element = document.querySelectorAll(selector);
+    if(element.length > 0)
+        return element[0];
+    return null;
+};
 
 function hamClick(){
     const navShowClass = "nav-show";
-    const navSelector = document.querySelectorAll(".site-navigation");
-    if(navSelector.length > 0) {
-        const nav = navSelector[0];
+    let nav = queryElement(".site-navigation");
+    if(nav !== null) {
         if(nav.className.indexOf(navShowClass) > -1) {            
             nav.className = nav.className.replace(navShowClass, "");
         }
@@ -14,8 +24,25 @@ function hamClick(){
     }
 }
 
+function ClaculateBlogHeigh() {
+    const height = window.innerHeight;
+    let main = queryElement("body > main");
+    if(main !== null){
+        if(main.className.indexOf("home") === -1){
+            const headerHeight = 115;
+            const totalHeight = height - headerHeight;
+            main.style = "min-height: " + totalHeight + "px";
+        }
+    }
+}
+/** End Functions */
+
+// set hambuguer actions
 const hamburguerSelector = document.querySelectorAll(".hamburguer");
 if(hamburguerSelector.length > 0) {
     const hamburguer = hamburguerSelector[0];
     hamburguer.addEventListener("click", hamClick);
 }
+
+// Set height
+ClaculateBlogHeigh();
--- index.md
+++ index.md
@@ -1,18 +1,13 @@
 ---
 layout: default
+class: "home"
 ---
 
 Here you will see an image or something else 
 
-## Latest posts
+<!-- ## Latest posts -->
 
 
-
-<div>
-  {% for post in site.posts %}
-    <div>
-      <h3 class="title"><a href="{{ post.url }}">{{ post.title }}</a></h3>
-      <p>{{ post.excerpt }}</p>
-    </div>
-  {% endfor %}
+<div style="width: 300px; height: 200px; margin: 300px auto;">
+  An image here
 </div>
